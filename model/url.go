860caeb1298fa18a867ead9d50a7e91ef4224e9c
Richard Burk Orofeo <chardyy.orofeo@gmail.com>
2020-07-10 00:07:25 +0800

    added model for URL

---
+++ model/url.go
@@ -0,0 +1,18 @@
+package model
+
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+// URL represents a url to be
+// shortened.
+type URL struct {
+	ID        primitive.ObjectID `bson:"_id" json:"_id"`
+	Alias     string             `bson:"alias" json:"alias"`
+	URL       string             `bson:"url" json:"url"`
+	Click     int32              `bson:"click" json:"click"`
+	CreatedAt time.Time          `bson:"created_at" json:"created_at"`
+	UpdatedAt time.Time          `bson:"updated_at" json:"updated_at"`
+}
